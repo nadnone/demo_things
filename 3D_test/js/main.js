de3314b0691cbca4b11dbdcb1bb27bc306f8ff08
Nad <34311347+nadnone@users.noreply.github.com>
2022-08-16 01:23:15 +0200

    update :)

--- 3D_test/js/main.js
+++ 3D_test/js/main.js
@@ -7,57 +7,68 @@ import drawFunction from './tools/drawFunction.js';
 import { cube, colors } from './data/cube.js';
 import { multiply } from './tools/vectors_maths.js';
 
-function main()
+
+let i = 0;
+
+function loop()
 {
 
-    ctx.clearRect(0,0, WIDTH, HEIGHT);
+        ctx.clearRect(0,0, WIDTH, HEIGHT);
 
-    // rotation
-
-    const angle = 1 * Math.PI / 180;
-
-    const cos = Math.cos(angle);
-    const sin = Math.sin(angle);
-
-    const MATRIX_ROT_X = [
-        [1, 0, 0],
-        [0, cos, -sin],
-        [0, sin, cos]
-    ];
-    const MATRIX_ROT_Y = [
-        [cos, 0, sin],
-        [0,  1, 0],
-        [-sin, 0, cos]
-    ];
-    const MATRIX_ROT_Z = [
-        [cos, -sin, 0],
-        [sin, cos, 0],
-        [0, 0, 1]
-    ];
-
-    let matrice_transform = cube;
-
-    for (let j = 0; j < cube.length; j+=3) {
-
-        let buffer = kernel_get(j, matrice_transform);
-
-        buffer = multiply(buffer, MATRIX_ROT_X)
-        buffer = multiply(buffer, MATRIX_ROT_Y)
-        //buffer = multiply(buffer, MATRIX_ROT_Z)
-
-        for (let k = 0; k < 3; k++) {
-            matrice_transform[j+k] = buffer[k]
-        }
-
-    }
+        // rotation
     
+        let angle = i * Math.PI / 180;
+        angle %= Math.PI*2
+    
+        const cos = Math.cos(angle);
+        const sin = Math.sin(angle);
+    
+        const MATRIX_ROT_X = [
+            [1, 0, 0],
+            [0, cos, -sin],
+            [0, sin, cos]
+        ];
+        const MATRIX_ROT_Y = [
+            [cos, 0, sin],
+            [0,  1, 0],
+            [-sin, 0, cos]
+        ];
+        const MATRIX_ROT_Z = [
+            [cos, -sin, 0],
+            [sin, cos, 0],
+            [0, 0, 1]
+        ];
+    
+        let matrice_transform = [];
+    
+        for (let j = 0; j < cube.length; j+=3) {
+    
+            let buffer = kernel_get(j, cube);
+    
+            buffer = multiply(buffer, MATRIX_ROT_X)
+            buffer = multiply(buffer, MATRIX_ROT_Y)
+            buffer = multiply(buffer, MATRIX_ROT_Z)
+    
+            for (let k = 0; k < 3; k++) {
+                matrice_transform[j+k] = buffer[k]
+            }
+    
+        }
+        
+    
+        const matrice_totale = projection(matrice_transform);
+        graphics_pipeline(matrice_totale, colors);
 
-    let matrice_totale = projection(matrice_transform);
+        
 
-    graphics_pipeline(matrice_totale, colors);
+        i += 2
+
+        matrice_transform = cube;
 
 
 
+
+    
 
 }
 
@@ -72,5 +83,5 @@ function kernel_get(cursor, matrice)
     return m_out;
 }
 
-//main();
-setInterval(main, 60);
+
+setInterval(loop, 60);
--- 3D_test/js/tools/graphics_pipeline.js
+++ 3D_test/js/tools/graphics_pipeline.js
@@ -1,28 +1,20 @@
 import { ctx, HALF_HEIGHT, HALF_WIDTH } from './constants.js';
 import drawFunction from './drawFunction.js';
-import { soustraction, determinant_3x3, norme, angle_vector, produit_vectoriel, produit_scalair} from './vectors_maths.js';
+import { soustraction, produit_vectoriel, produit_scalair, scalair} from './vectors_maths.js';
 
-export default function graphics_pipeline(m, colors) 
+export default function graphics_pipeline(m, colors, angle) 
 {
 
     for (let i = 0; i < m.length; i+=3) {
-       
-
+    
         const V0 = m[i + 0]
         const V1 = m[i + 1]
         const V2 = m[i + 2]
         
 
-        /*
-            backface detection pour supprimer les faces arrières
-            
-            https://youtu.be/ShTiQGxiZRk
-            la normale de la face du triangle * le vecteur de vue de la caméra (dans mon cas, V0)
-        */
-        const backtest = produit_scalair( produit_vectoriel( soustraction(V1, V0), soustraction(V2, V0) ), V0 );
-        if (backtest > 0 ) continue;
-    
-        
+        // if triangle is behind the projection view
+        //if (!IsBehind(V0, V1, V2)) continue
+
 
         // to check less pixels
         const min_x = Math.min(V0[0], V1[0], V2[0]);
@@ -37,15 +29,15 @@ export default function graphics_pipeline(m, colors)
             for (let py = min_y; py <= max_y; py++) 
             {
     
-                let p = [px, py]
- 
-   
+                const p = [px, py]
+
+                
+
                 // edge detection pour savoir si le pixel est dans le triangle 
                 if (isPointInTriangle(p, V0, V1, V2))
                 {
                     drawFunction(px + HALF_WIDTH, py + HALF_HEIGHT, colors[i])
                 }
-                    
             }
         }
 
@@ -54,20 +46,54 @@ export default function graphics_pipeline(m, colors)
 
 }
 
-function isPointInTriangle(p, a, b, c)
+function IsBehind(a,b,c)
 {
-    // on teste tous les côtés
-    return isInside(b,c, p) && isInside(c,a, p) &&  isInside(a,b, p)
+    return backface_culling(a,b,c)
 }
 
-function isInside(a, b, c)
+function backface_culling(a,b,c)
 {
+    // https://en.wikipedia.org/wiki/Back-face_culling
+
     // A REVOIR
 
+    let delta_AB = soustraction(b,a)
+    let delta_AC = soustraction(c,a)
 
-    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]) >= 0
+    const N = produit_vectoriel(delta_AB, delta_AC)
+
+    let view = scalair(a, -1)
+
+    let calc0 = produit_scalair(view, N) >= 0
+
+    return calc0 
+}
+
+function isPointInTriangle(p, a, b, c)
+{
+    // on teste tous les côtés positif et négatifs
+
+    let check1 = isInside(c,a, p) > 0
+    check1 &= isInside(a,b, p) > 0
+    check1 &= isInside(b,c, p) > 0
+
+    let check2 = isInside(c,a, p) < 0
+    check2 &= isInside(a,b, p) < 0
+    check2 &= isInside(b,c, p) < 0
+
+
+    
+
+    return check1 | check2
+}
+
+function isInside(a, b, p)
+{
+
+    return (a[0] - p[0]) * (b[1] - p[1]) - (a[1] - p[1]) * (b[0] - p[0])
 
     // solution ici
+    // https://youtu.be/kkucCUlyIUE
     // https://blackpawn.com/texts/pointinpoly/default.html
     // https://fgiesen.wordpress.com/2013/02/08/triangle-rasterization-in-practice/
 
